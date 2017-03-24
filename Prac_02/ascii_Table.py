821bd9e0f014728b36e424f5675f39f25c311ca4
Nando <nando.retallickboundey@my.jcu.edu.au>
2017-03-24 12:43:54 +1000

    add functions for number

--- Prac_02/ascii_Table.py
+++ Prac_02/ascii_Table.py
@@ -1,20 +1,33 @@
 MIN_CHARACTER_CODE = 33
 MAX_CHARACTER_CODE = 127
 
-character_to_convert = str(input("Enter a character: "))
-character_code = ord('{}'.format(character_to_convert))
 
-print("The ASCII code for {} is {}".format(character_to_convert, character_code))
+def main():
+    value = int(get_number(MIN_CHARACTER_CODE, MAX_CHARACTER_CODE))
+    character_to_convert = str(input("Enter a character: "))
+    character_code = ord('{}'.format(character_to_convert))
 
-code_to_convert = int(input("Enter a number between {} and {}: ".format(MIN_CHARACTER_CODE, MAX_CHARACTER_CODE)))
-code_character = chr(code_to_convert)
+    print("The ASCII code for {} is {}".format(character_to_convert, character_code))
 
-while code_to_convert < MIN_CHARACTER_CODE or code_to_convert > MAX_CHARACTER_CODE:
-    print("Invalid input please try again")
-    code_to_convert = int(input("Enter a number between {} and {}: ".format(MIN_CHARACTER_CODE, MAX_CHARACTER_CODE)))
-    code_character = chr(code_to_convert)
+    value_character = chr(value)
 
-print("The character for {} is {}".format(code_to_convert, code_character))
+    print("The character for {} is {}".format(value, value_character))
 
-for i in range(33, 127):
-    print("{:4} {:>4}".format(i, chr(i)))
+    # for i in range(33, 127):
+    #    print("{:4} {:>4}".format(i, chr(i)))
+
+
+def get_number(lower, upper):
+    while True:
+        try:
+            number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
+            while number < lower or number > upper:
+                print("Invalid number")
+                number = int(input("Enter a number between {} and {}: ".format(lower, upper)))
+            else:
+                return number
+        except ValueError:
+            print("Invalid input, must be a number")
+
+
+main()
