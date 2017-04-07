cc4ab8db834dd34d9662f523cd162ccde2c18edc
NandoRetallick-Boundey <nretallick@y7mail.com>
2017-04-07 13:52:11 +1000

    add Hexadecimal_Colour checker fixed state_Names vlaue checker, PEP8 convensions

---
+++ Prac_6/Hexadecimal_Colours.py
@@ -0,0 +1,16 @@
+"""
+Author: Nando Retallick-Boundey
+CP1404 prac
+Hexadecimal colour conversion
+"""
+
+HEXADECIMAL_COLOUR = {"RED": "#FF0000", "GREEN": "#008000", "BLUE": "#0000FF",
+                      "YELLOW": "#FFFF00", "GOLD": "#FFD700", "SILVER": "#C0C0C0",
+                      "SNOW": "#FFFAFA", "IVORY": "#FFFFF0", "WHEAT": "#F5DEB3",
+                      "STEELBLUE": "#4682B4"}
+
+colour = input("Enter a colour: ").upper()
+while colour != "":
+    if colour in HEXADECIMAL_COLOUR:
+        print("{:10} is {}".format(colour, HEXADECIMAL_COLOUR[colour]))
+    colour = input("Enter a colour: ").upper()
