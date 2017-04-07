8f0ed94d3f9b76bbbd341a100e65c4fc413002b8
NandoRetallick-Boundey <nretallick@y7mail.com>
2017-04-07 13:54:48 +1000

    fixed PEP8 convensions

--- Prac_6/State_Names.py
+++ Prac_6/State_Names.py
@@ -4,7 +4,6 @@ State names in a dictionary
 File needs reformatting
 """
 
-
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
